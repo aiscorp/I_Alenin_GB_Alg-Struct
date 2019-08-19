b717aa0d3ac584b05ef20345bf18c4e2cff23b02
Ivan Alenin <aiscorp@yandex.ru>
2019-08-20 00:47:26 +0300

    Задание 1 готово

--- Lesson_2/Lesson_2/Program.cs
+++ Lesson_2/Lesson_2/Program.cs
@@ -6,8 +6,42 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // =============================
+            Console.WriteLine("Ivan Alenin - Lesson 2 \n1. Decimal to binary.");
+            // =============================
+            // Lesson 2, Task 1
+            bool result; int dec_val;
+            do
+            {
+                Console.WriteLine("Please type decimal value: ");
+                result = int.TryParse(Console.ReadLine(), out dec_val);
+            } while (result == false);
+            DecToBin(dec_val);
+            // =============================
+            // Lesson 2, Task 2
+
+
+            // =============================
             Console.ReadLine();
+        }
+
+        // Lesson 2, Task 1
+        static void DecToBin(int decimal_val)
+        {
+            Console.Write("Decimal: {0}, Binary: 0b", decimal_val.ToString());
+            byte nothing = DecToBin_Rec(decimal_val);
+        }
+
+        static byte DecToBin_Rec(int decimal_val)
+        {
+            if (decimal_val != 0)
+            {
+                DecToBin_Rec(decimal_val / 2);
+            }
+            else return 0;
+
+            Console.Write((decimal_val % 2).ToString());
+            return 0;
         }
     }
 }
