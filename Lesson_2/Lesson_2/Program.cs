33be2fd9b2c180eb2c65a648b8a55741ffef863e
aiscorp <43959850+aiscorp@users.noreply.github.com>
2019-08-20 14:15:27 +0300

    Revert "Lesson 2"

--- Lesson_2/Lesson_2/Program.cs
+++ Lesson_2/Lesson_2/Program.cs
@@ -20,24 +20,19 @@ namespace Lesson_2
             // =============================
             // Lesson 2, Task 2
             Console.WriteLine("\n \n2. Exponentiation positive integer number.");
-            ulong number, power;
+            int number, power;
             do
             {
                 Console.WriteLine("Please enter a positive integer number: ");
-                result = ulong.TryParse(Console.ReadLine(), out number);
+                result = int.TryParse(Console.ReadLine(), out number);
             } while (result == false);
             do
             {
                 Console.WriteLine("Please enter its positive integer degree: ");
-                result = ulong.TryParse(Console.ReadLine(), out power);
+                result = int.TryParse(Console.ReadLine(), out power);
             } while (result == false);
-
-            ulong power_result = PowNumber(number, power);
-            Console.WriteLine("Result whith a FOR loop: {0}", power_result.ToString());
-            Console.WriteLine();
-
-            power_result = PowNumberRec(number, power);
-            Console.WriteLine("Result whith a recursion: {0}", power_result.ToString());
+            ulong power_result = (ulong)PowNumber(number, power);
+            Console.Write(power_result.ToString());
 
             // =============================
             Console.ReadLine();
@@ -48,7 +43,7 @@ namespace Lesson_2
         {
             Console.Write("Decimal: {0}, Binary: 0b", decimal_val.ToString());
             byte nothing = DecToBin_Rec(decimal_val);
-            // Console.Write("\n");
+            Console.Write("\n");
         }
 
         static byte DecToBin_Rec(int decimal_val)
@@ -63,38 +58,14 @@ namespace Lesson_2
             return 0;
         }
         // Lesson 2, Task 2
-        static ulong PowNumber(ulong number, ulong power)
+        static int PowNumber(int number, int power)
         {
-            ulong result = number;
-            for (int i = 1; i < (int)power; i++)
+            int result = number;
+            for (int i = 1; i < power; i++)
             {
                 result *= number;
-                Console.WriteLine("result={0}, power={1}", result, power);
             }
             return result;
-        }
-        // Lesson 2, Task 2
-        static ulong PowNumberRec(ulong number, ulong power)
-        {
-            ulong result = number;
-            if (power > 1)
-            {
-                if (power % 2 != 0)
-                {
-                    power--;
-                    result = PowNumberRec(number, power);
-                    Console.WriteLine("result={0}, power={1}", result, power);
-                    return result * number;
-                }
-                else
-                {
-                    power /= 2;
-                    result = PowNumberRec(number, power);
-                    Console.WriteLine("result={0}, power={1}", result, power);
-                    return result * result;
-                }
-            }
-            return result; 
         }
     }
 }
